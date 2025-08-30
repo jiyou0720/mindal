d8fddcd5a0e8be7123705853866697ddc065026d
jiyou0720 <145911634+jiyou0720@users.noreply.github.com>
2025-08-31 01:30:38 +0900

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,7 @@
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_pymongo import PyMongo
 
 db = SQLAlchemy()
-# FIX: Add the missing Migrate object instance
 migrate = Migrate()
 mongo = PyMongo()
