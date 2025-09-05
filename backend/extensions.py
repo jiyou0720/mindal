2c22dd8e716efd30d17b8de4d0bac553d3f9e9a0
jiyou0720 <145911634+jiyou0720@users.noreply.github.com>
2025-09-06 01:55:16 +0900

    Update extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -2,12 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt  # 1. Bcrypt 라이브러리를 가져옵니다.
 
-# 각 Flask 확장(extension)의 인스턴스를 생성합니다.
-# 이 인스턴스들은 app.py의 create_app 함수 내에서
-# 애플리케이션과 연결(init_app)됩니다.
-
+# 각 Flask 확장 기능의 인스턴스를 생성합니다.
+# 이 인스턴스들은 app.py에서 애플리케이션과 연결됩니다.
 db = SQLAlchemy()
 mongo = PyMongo()
 migrate = Migrate()
-jwt = JWTManager() # JWTManager 인스턴스 추가
+jwt = JWTManager()
+bcrypt = Bcrypt()              # 2. bcrypt 인스턴스를 생성하여 다른 파일에서 사용할 수 있도록 합니다.
+
